91d292205b17c7f3c6d23c4a3608b170a91821d2
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-12-16 18:33:44 +0100

    mejoradas + producto

--- operaciones.py
+++ operaciones.py
@@ -1,15 +1,38 @@
 def suma(a, b):
     try:
-        a + b
-    except TypeError:
+        a == float(a) and b == float(b)
+    except ValueError:
         return 'Error: Tipo de dato no válido'
     else:
-        return a + b
+        try:
+            a + b
+        except TypeError:
+            return 'Error: Tipo de dato no válido'
+        else:
+            return a + b
 
 def resta(a, b):
     try:
-        a - b
-    except TypeError:
+        a == float(a) and b == float(b)
+    except ValueError:
         return 'Error: Tipo de dato no válido'
     else:
-        return a - b
+        try:
+            a - b
+        except TypeError:
+            return 'Error: Tipo de dato no válido'
+        else:
+            return a - b
+
+def producto(a, b):
+    try:
+        a == float(a) and b == float(b)
+    except ValueError:
+        return 'Error: Tipo de dato no válido'
+    else:
+        try:
+            a * b
+        except TypeError:
+            return 'Error: Tipo de dato no válido'
+        else:
+            return a * b
