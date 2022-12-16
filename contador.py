3d8fee41379fa9de1d15adfc330337945617c8cd
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-12-16 19:11:43 +0100

    ejr2

--- contador.py
+++ contador.py
@@ -1,5 +1,24 @@
-f = open('contador.txt', "a+", encoding="utf-8")
-f.seek(0)
-if f.read() == '':
-    f.write('0')
-f.close
+def ejr2():
+    f = open('contador.txt', "a+", encoding="utf-8")
+    f.seek(0)
+    if f.read() == '':
+        f.write('0')
+    f.seek(0)
+    valor = f.read()
+    try:
+        valor == int(valor)
+    except:
+        print('Error: Fichero corrupto')
+    else:
+        argumento = input('Escriba "inc" o "dec" para incrementar o decrementar el contador: ')
+        if argumento == 'inc':
+            f.truncate(0)
+            f.write(str(int(valor) + 1))
+        elif argumento == 'dec':
+            f.truncate(0)
+            f.write(str(int(valor) - 1))
+        else:
+            print(valor)
+        f.close
+
+ejr2()
