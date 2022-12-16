0bc76941693e25f2ae4e4c8bea3fb20b399a8ed3
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-12-16 18:43:31 +0100

    division

--- operaciones.py
+++ operaciones.py
@@ -1,6 +1,6 @@
 def suma(a, b):
     try:
-        a == float(a) and b == float(b)
+        a == float(a) or b == float(b)
     except ValueError:
         return 'Error: Tipo de dato no válido'
     else:
@@ -13,7 +13,7 @@ def suma(a, b):
 
 def resta(a, b):
     try:
-        a == float(a) and b == float(b)
+        a == float(a) or b == float(b)
     except ValueError:
         return 'Error: Tipo de dato no válido'
     else:
@@ -36,3 +36,21 @@ def producto(a, b):
             return 'Error: Tipo de dato no válido'
         else:
             return a * b
+    
+def division(a, b):
+    try:
+        a == float(a) and b == float(b)
+    except ValueError:
+        return 'Error: Tipo de dato no válido'
+    else:
+        try:
+            a/b
+        except ZeroDivisionError:
+            return 'Error: No es posible dividir entre cero'
+        else:
+            try:
+                a/b
+            except TypeError:
+                return 'Error: Tipo de dato no válido'
+            else:
+                return a/b
